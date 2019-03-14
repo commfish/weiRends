395df151f3a89bbefa96807872b594ab02ea16a5
Ben <ben.williams@alaska.gov>
2019-03-14 14:01:20 -0800

    init commit

---
+++ code/helper.r
@@ -0,0 +1,10 @@
+# libraries 
+library(tidyverse)
+library(lubridate)
+library(broom)
+library(nls.multstart) # devtools::install_github('padpadpadpad/nls.multstart')
+library(nlstools)
+library(scales)
+library(FNGr) # devtools::install_github('ben-williams/FNGr')
+theme_set(theme_sleek())
+library(MASS)
