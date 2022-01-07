4e25ae92329643a944b3beef59ad55801be3cc37
Miller <sara.miller@dfg.alaska.local>
2022-01-07 09:24:11 -0900

    Changes Committed

--- code/functions_Situk.R
+++ code/functions_Situk.R
@@ -147,7 +147,7 @@ f_table_output <- function (preds){ # total raw count compared to fitted count
   
   preds %>% 
     group_by(year) %>%
-    summarise(total_count = max(cumsum, na.rm = T),
+    summarise(count = max(cumsum, na.rm = T),
               fitted = max(fit_cumsum, na.rm = T)) %T>%
     write_csv(., paste0('output/', folder, '/summary_table.csv'))}
 
@@ -204,6 +204,8 @@ plot1
 }
 
 
+
+
 f_run_through <- function(preds, perc = 0.95, prob = 0.95){ #change perc to 0.90 for alt run
 
   preds %>% 
@@ -219,6 +221,10 @@ f_run_through <- function(preds, perc = 0.95, prob = 0.95){ #change perc to 0.90
     write_csv(., paste0('output/', folder, '/run_through.csv'))
   
 }
+
+
+
+
 
 f_run95 <- function(preds, run_through, perc = 0.95){ #change perc to 0.90 for alt run
   run_through = run_through$end_date
@@ -606,4 +612,232 @@ f_median_end_date <- function(remove_dates, low = .25, high = .75){
 
 }
 
+# Impute Data
+# These functions were written by Justin Priest (ADF&G biologist; Douglas, AK)
+
+
+# There are four functions here:
+# impute_global() which imputes all NAs in all years iteratively
+# impute_cohodefault() which imputes all NAs before year 2000 globally, then imputes annually
+# impute_local() which imputes a 10-year rolling imputation (prev & following 5 years)
+# impute_local_improved which is similar to impute_local() but accounts for early years better
+
+impute_global <- function(dfname, Year_column="year", StreamName_column="stream_name",
+                          outputname = "globalimpute", # Only used if Step 3 turned "on"
+                          Count_column = "count"){
+  ### SUMMARY: Global Impute ###
+  # This creates a dataframe that imputes NA values.
+  # This algorithm interpolates across rows and columns, following Blick
+  # In essence, imputing across rows (years) and columns (streams) allows for an NA in
+  #  a year/stream to be informed by typical counts and for that year AND stream
+  # This function can be easily modified to auto-create a named dataframe
+  
+  # Make sure that all NAs are present (a missing row is NOT same as a row with an NA)
+  
+  
+  ### EXAMPLE USAGE ###
+  # impute_global(ktn_index, Year_column="year")
+  
+  # Step 1: Set up dataframe to impute
+  require(dplyr)
+  .test <- dfname %>% rename(year = Year_column, stream_name = StreamName_column, count = Count_column)
+  .test <- .test %>% dplyr::select(year, stream_name, count)
+  .test <- .test %>% mutate(imputed = is.na(count))
+  
+  # Step 2: Use multiplicative imputation as per Blick, in an iterative procedure
+  j=1
+  repeat{
+    for(i in 1:nrow(.test)){
+      .temprow = .test[i,]
+      
+      if(.temprow$imputed == TRUE){
+        .sumyr = sum((.test %>% filter(year == .temprow$year) )$count, na.rm = TRUE)
+        .sumrvr = sum((.test %>% filter(stream_name == .temprow$stream_name) )$count, na.rm = TRUE)
+        .sumall = sum(.test$count, na.rm = TRUE)
+        .test$count[i] = .sumyr * .sumrvr / .sumall
+        # this interpolates across rows and columns
+      }
+    }
+    j=j+1
+    if(j>100){break} # repeat the above 100 times
+  }
+  #print(.test)
+  
+  # Optional Step 3: Auto create a dataframe with the correct name
+  #assign(paste0((outputname), "_survey_imputed"), .test, envir = parent.frame() ) # use if you want a dynamic name
+  imputedsurvey <- .test # use this if you want a static name
+  write.csv(imputedsurvey, paste0('output/', folder, '/EM_algorithm_model.csv'))
+}
+
+
+
+
+impute_cohodefault <- function(dfname, Year_column="year", StreamName_column="stream_name",
+                               Count_column = "count", outputname = "defaultcohoimputed"){
+  ### SUMMARY: Impute 1987-2000 globally, then backwards impute each new year ###
+  # This takes a dataframe with NA values and imputes missing data
+  
+  # This algorithm is the default method Leon used for imputation.
+  # For all years pre-2000, impute globally (all values 1987-1999 inform imputed NAs)
+  # Then for each following year calculate a new imputation looking backwards only
+  # Leon imputed everything once ~2000 then annually would add the new years data with blanks
+  # He ran the imputation on this new row which was informed by previous years' imputations
+  # Once imputed, these values would go in the table and not be updated
+  # Make sure that all NAs are present (a missing row is NOT same as a row with an NA)
+  
+  ### EXAMPLE USAGE ###
+  # impute_cohodefault(ktn_index, Year_column="year", outputname = "ktn2020")
+  
+  
+  # Step 1: Set up dataframe to impute
+  require(dplyr)
+  .test <- dfname %>% rename(year = Year_column, stream_name = StreamName_column, count = Count_column)
+  .test <- .test %>% dplyr::select(year, stream_name, count)
+  .test <- .test %>% mutate(imputed = is.na(count))
+  
+  # Step 2: Use multiplicative imputation as per Blick, in an iterative procedure
+  
+  j=1
+  repeat{
+    for(i in 1:nrow(.test %>% filter(year < 2000))){
+      .temprow = .test[i,]
+      if(.temprow$imputed == TRUE){
+        .test_early <- .test %>% filter(year < 2000)
+        .sumyr = sum((.test_early %>% filter(year == .temprow$year) )$count, na.rm = TRUE)
+        .sumrvr = sum((.test_early %>% filter(stream_name == .temprow$stream_name) )$count, na.rm = TRUE)
+        .sumall = sum(.test_early$count, na.rm = TRUE)
+        .test$count[i] = .sumyr * .sumrvr / .sumall
+      }
+    }
+    j=j+1
+    if(j>100){break} # repeat the above 100 times. Needs to be iterative (imputing depends on other imputed values)
+  }# end early
+  
+  j=1
+  repeat{
+    for(i in (nrow(.test %>% filter(year < 2000))+1):nrow(.test)){
+      .temprow = .test[i,]
+      if(.temprow$imputed == TRUE){
+        .curryr <- .temprow$year
+        .yr_range = .test %>% filter(year <= .curryr) #5 yrs before / after
+        .sumyr = sum((.yr_range %>% filter(year == .temprow$year) )$count, na.rm = TRUE)
+        .sumrvr = sum((.yr_range %>% filter(stream_name == .temprow$stream_name) )$count, na.rm = TRUE)
+        .sumall = sum(.yr_range$count, na.rm = TRUE)
+        .test$count[i] = .sumyr * .sumrvr / .sumall
+        # this is multiplicative imputation as per Blick
+      }
+    }
+    j=j+1
+    if(j>100){break} # repeat the above 100 times. Needs to be iterative (imputing depends on other imputed values)
+  }
+  print(.test)
+  # Step 3: Create the dataframe with the correct name
+  # assign(paste0((outputname), "_survey_imputed"), .test, envir = parent.frame() ) # use if you want a dynamic name
+  # imputedsurvey <- .test # use this if you want a static name
+}
+
+
+
+impute_local <- function(dfname, Year_column="year", StreamName_column="stream_name",
+                         Count_column = "count"){
+  ### SUMMARY: 10-yr Localized Imputation ###
+  # This takes a dataframe with NA values and imputes missing data
+  # This algorithm uses "local" imputation: only 5 years before and after impute a missing value
+  # i.e., only using the preceding 5 years and following 5 years
+  # Make sure that all NAs are present (a missing row is NOT same as a row with an NA)
+  
+  ### EXAMPLE USAGE ###
+  # impute_local(ktn_index, Year_column="year")
+  
+  # Step 1: Set up dataframe to impute
+  require(dplyr)
+  .test <- dfname %>% rename(year = Year_column, stream_name = StreamName_column, count = Count_column)
+  .test <- .test %>% dplyr::select(year, stream_name, count)
+  .test <- .test %>% mutate(imputed = is.na(count))
+  
+  # Step 2: Use multiplicative imputation as per Blick, in an iterative procedure
+  
+  j=1
+  repeat{
+    for(i in 1:nrow(.test)){
+      .temprow = .test[i,]
+      
+      if(.temprow$imputed == TRUE){
+        .yr_range = .test %>% filter(between(year, .temprow$year - 5, .temprow$year + 5)) #5 yrs before / after
+        .sumyr = sum((.yr_range %>% filter(year == .temprow$year) )$count, na.rm = TRUE)
+        .sumrvr = sum((.yr_range %>% filter(stream_name == .temprow$stream_name) )$count, na.rm = TRUE)
+        .sumall = sum(.yr_range$count, na.rm = TRUE)
+        .test$count[i] = .sumyr * .sumrvr / .sumall
+        # this is multiplicative imputation as per Blick
+      }
+    }
+    j=j+1
+    if(j>100){break} # repeat the above 100 times
+  }
+  print(.test)
+  
+  # Optional Step 3: Auto create a dataframe with the correct name
+  # assign(paste0((outputname), "_survey_imputed"), .test, envir = parent.frame() ) # use if you want a dynamic name
+  # imputedsurvey <- .test # use this if you want a static name
+  
+}
+
+
+
+impute_local_improved <- function(dfname, Year_column="year", StreamName_column="stream_name",
+                                  Count_column = "count"){
+  ### SUMMARY: 10-yr Localized Imputation, improved ###
+  # This takes a dataframe with NA values and imputes missing data
+  # This algorithm uses "local" imputation: only 5 years before and after impute a missing value
+  # i.e., only using the preceding 5 years and following 5 years
+  # However this version adds a rule for early years (1987-1996) to use 10 next years (10 yr minimum)
+  # Make sure that all NAs are present (a missing row is NOT same as a row with an NA)
+  
+  ### EXAMPLE USAGE ###
+  # impute_local_improved(ktn_index, Year_column="year")
+  
+  # Step 1: Set up dataframe to impute
+  require(dplyr)
+  .test <- dfname %>% rename(year = Year_column, stream_name = StreamName_column, count = Count_column)
+  .test <- .test %>% dplyr::select(year, stream_name, count)
+  .test <- .test %>% mutate(imputed = is.na(count))
+  
+  # Step 2: Use multiplicative imputation as per Blick, in an iterative procedure
+  
+  j=1
+  repeat{
+    for(i in 1:nrow(.test)){
+      .temprow = .test[i,]
+      
+      if(.test$year < 1997){
+        if(.temprow$imputed == TRUE){
+          .test_early <- .test %>% filter(year < 1997)
+          .sumyr = sum((.test_early %>% filter(year == .temprow$year) )$count, na.rm = TRUE)
+          .sumrvr = sum((.test_early %>% filter(stream_name == .temprow$stream_name) )$count, na.rm = TRUE)
+          .sumall = sum(.test_early$count, na.rm = TRUE)
+          .test$count[i] = .sumyr * .sumrvr / .sumall
+        } # end early
+      }}
+    j=j+1
+    if(j>50){break} # repeat the above 50 times. Needs to be iterative (imputing depends on other imputed values)
+  }# end early
+  
+  j=1
+  repeat{
+    for(i in (nrow(.test %>% filter(year < 1997))+1):nrow(.test)){
+      .temprow = .test[i,]
+      if(.temprow$imputed == TRUE){
+        .yr_range = .test %>% filter(between(year, .temprow$year - 5, .temprow$year + 5)) #5 yrs before / after
+        .sumyr = sum((.yr_range %>% filter(year == .temprow$year) )$count, na.rm = TRUE)
+        .sumrvr = sum((.yr_range %>% filter(stream_name == .temprow$stream_name) )$count, na.rm = TRUE)
+        .sumall = sum(.yr_range$count, na.rm = TRUE)
+        .test$count[i] = .sumyr * .sumrvr / .sumall
+        # this is multiplicative imputation as per Blick
+      }
+    }
+    j=j+1
+    if(j>50){break} # repeat the above 50 times. Needs to be iterative (imputing depends on other imputed values)
+  } # end late
+  print(.test)
+}
 
--- code/situk_analysis.r
+++ code/situk_analysis.r
@@ -3,17 +3,16 @@
 
 # ben.williams@alaska.gov
 # 2019-03
-# last updated by SEM: October 15, 2021
+# last updated by SEM: January 5, 2022
 # Notes:
 # 1. calculate end date of weir deployment
-# 2. model tails of the runs, by year - Gompertz or logistic model
+# 2. model tails of the runs, by year - Gompertz or logistic model or Missfill
 # 3. estimate the date required to operate through so that 95% of the escapement on average is observed
 # 4. weir 1% rule - based upon 5,4, 3, or 2 days meeting the 1% requirement
 
 # load ----
 source('code/helper.r')
 source('code/functions_Situk.r')
-
 # globals ----
 folder <- 'situk'
 
@@ -44,11 +43,12 @@ readRDS(paste0('output/', folder, '/logistic_model.rda')) -> model_logistic
 f_summary(model_gompertz)
 f_summary(model_logistic)
 
+
 # which model performs better
 # >0.50 = model 1 (model_gompertz)
 # <0.50 = model 2 (model_logistic)
 f_deviance(model_gompertz, model_logistic) # check model fits - did all models converge?
-# need to change all references to correct model after 'best' model is chosen
+# need to change all references to correct model (below) after 'best' model is chosen
 
 # get parameters
 f_params(model_gompertz) -> params # choose model based on deviance
@@ -58,6 +58,72 @@ f_param_plot(params)
 
 # predict the model on a complete dataset
 f_preds(df, model_gompertz) -> preds #preds.csv
+
+# table of data cumsum (raw data) and fit_cumsum
+f_table_output(preds) # total count of raw versus fitted; summary_table.csv
+
+# plot of data cumsum (raw data) and fit_cumsum
+tickryr <- data.frame(year = 1985:2025)
+axisf <- tickr(tickryr, year, 5)
+f_plot_output(preds) # fitted_plot.png
+
+# what is the minimum day that the weir should be in place?
+# the 95th percentile of the Julian date that 95% of the modeled run has been observed in the last 10 years only - 
+f_run_through(preds) -> run_through # run_through.csv file
+
+# plot the predicted data and fits 
+f_run95(preds, run_through) # outputs the 95% cumsum by year as a Julian date; run_95.csv
+f_pred_plot(preds, run_through) # pred_plot.png
+f_pred1999_plot(preds, run_through) # pred_plot1990.png
+f_pred2000_plot(preds, run_through) # pred_plot1991.png
+f_pred2010_plot(preds, run_through) # pred_plot2001.png
+
+# dates the weirs would be removed based upon 1%  rule
+# for 5,4,3, or 2 days
+f_remove_dates(preds, run_through) -> remove_dates 
+f_remove_dates_table(preds, run_through) # remove_dates_table.csv
+
+# percent of the run that is caught at a given risk level
+f_percent_missed (preds, remove_dates) # percent_missed.csv
+f_run_caught(preds, remove_dates) # remove_dates_run_caught.csv
+
+# plot of missed run and risk
+f_risk_plot(preds, remove_dates) # remove_dates_risk_plot.png
+
+# median, 25% and 75% quantiles of weir end date
+f_median_end_date(remove_dates)
+
+# save the ouputs from above in the appropriate folder before running the code below
+
+## global function implementation ##-----------------------------------------------------------------------------------------------------
+read_csv('data/situk_weir_1988-2021.csv') %>% 
+  filter(species=='Sockeye') %>% 
+  dplyr::select(date, count) -> situk
+year_num <-1988 # years to include in run_through (years >= to year_num)
+# run functions ----
+
+# format data
+f_clean_data(situk) -> df
+
+df %>%
+  mutate (stream_name = julian) %>%
+  dplyr::select(year, stream_name, count, date, Year, cumsum) %>%
+impute_global(., Year_column="year")
+
+read_csv('output/situk/EM_algorithm_model.csv') %>%
+  arrange(year, stream_name) %>%
+  group_by(.,year) %>% 
+  mutate(julian = stream_name,
+         fit_run = count,
+         fit_cumsum = cumsum(fit_run),
+         Year = factor(year)) %>%
+  dplyr::select(julian, year, Year, fit_run, fit_cumsum, Year) -> preds
+  
+
+df %>%
+  dplyr::select(julian, year, count, cumsum) %>%
+  merge(., preds) -> preds
+write_csv(preds, paste0('output/', folder, '/preds.csv'))  
 
 # table of data cumsum (raw data) and fit_cumsum
 f_table_output(preds) # total count of raw versus fitted; summary_table.csv
